309691a6993781303bbbc724dd6cb0859311de5f
Jitse-Jan <jitsejan@gmail.com>
2016-09-15 09:53:35 -0400

    Add images

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -28,15 +28,23 @@
 		<script type="text/javascript">
 			$( document ).ready(function() {
 				$( '.swipebox' ).swipebox();
-				
 				$('#subnav').affix({
 					offset: {
 					top: $('#subnav').offset().top,
 					bottom: $('footer').outerHeight(true) + $('.application').outerHeight(true) + 40
 					}
 				});
-				
 			});
 		</script>
+                <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-9074661-8', 'auto');
+  ga('send', 'pageview');
+
+              </script>
 	</body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,37 +8,39 @@
 		<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css');?>">
 	</head>
 	<body>
-		<div class="container">
-			<div class="row">
-				<div id="blog-head" class="container">
-					<h3><?php echo $this->config->item('slogan'); ?></h3>
-					<h5><?php echo $this->config->item('description'); ?></h5>
-				</div>
-			</div>
-		</div><!-- /container !-->
-		<div class="<container-fluid">
-			<header role="banner" data-spy="affix" data-offset-top="140">
-				<nav class="navbar navbar-default">
-					<div class="container">
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-responsive-collapse">
-			    				<span class="sr-only">Navigation</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</button>
-							<a class="navbar-brand" title="<?php echo $this->config->item('slogan');?>" href="<?php echo base_url('home'); ?>"><?php echo $this->config->item('sitename');?></a>
-						</div>
-						<div id="navbar-responsive-collapse" class="collapse navbar-collapse">
-							<ul id="menu-menu-1" class="nav navbar-nav">
-								<li class="<?=($this->uri->segment(1)==='home' || null === $this->uri->segment(1))?'active':''?>"><a href="<?php echo base_url('home'); ?>" >Home</a></li>
-								<li class="<?=($this->uri->segment(1)==='code')?'active':''?>"><a href="<?php echo base_url('code'); ?>" >Code</a></li>
-								<li class="<?=($this->uri->segment(1)==='tutorials')?'active':''?>"><a href="<?php echo base_url('tutorials'); ?>" >Tutorials</a></li>
-								<li class="<?=($this->uri->segment(1)==='photos')?'active':''?>"><a href="<?php echo base_url('photos'); ?>" >Photos</a></li>
-								<li class="<?=($this->uri->segment(1)==='about')?'active':''?>"><a href="<?php echo base_url('about'); ?>" >About</a></li>
-							</ul>
-						</div>
+		<div class="top-bar">
+			<div class="container">
+				<div class="row">
+					<div id="blog-head" class="container">
+						<h3><?php echo $this->config->item('slogan'); ?></h3>
+						<h5><?php echo $this->config->item('description'); ?></h5>
 					</div>
-				</nav>
-			</header>
-		</div><!-- /container-fluid !-->
+				</div>
+			</div><!-- /container !-->
+			<div class="<container-fluid">
+				<header role="banner" data-spy="affix" data-offset-top="140">
+					<nav class="navbar navbar-default">
+						<div class="container">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-responsive-collapse">
+				    				<span class="sr-only">Navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+								<a class="navbar-brand" title="<?php echo $this->config->item('slogan');?>" href="<?php echo base_url('home'); ?>"><?php //echo $this->config->item('sitename');?><img src="assets/images/JJ,s World.gif"/></a>
+							</div>
+							<div id="navbar-responsive-collapse" class="collapse navbar-collapse">
+								<ul id="menu-menu-1" class="nav navbar-nav">
+									<li class="<?=($this->uri->segment(1)==='home' || null === $this->uri->segment(1))?'active':''?>"><a href="<?php echo base_url('home'); ?>" ><img src="assets/images/home.gif" alt="Home"/></a></li>
+									<li class="<?=($this->uri->segment(1)==='code')?'active':''?>"><a href="<?php echo base_url('code'); ?>" ><img src="assets/images/code.gif" alt="Code"/></a></li>
+									<li class="<?=($this->uri->segment(1)==='tutorials')?'active':''?>"><a href="<?php echo base_url('tutorials'); ?>" ><img src="assets/images/tutorials.gif" alt="Tutorials"/></a></li>
+									<li class="<?=($this->uri->segment(1)==='photos')?'active':''?>"><a href="<?php echo base_url('photos'); ?>" ><img src="assets/images/photos.gif" alt="Photos"/></a></li>
+									<li class="<?=($this->uri->segment(1)==='about')?'active':''?>"><a href="<?php echo base_url('about'); ?>" ><img src="assets/images/about.gif" alt="About"/></a></li>
+								</ul>
+							</div>
+						</div>
+					</nav>
+				</header>
+			</div><!-- /container-fluid !-->
+		</div><!-- /top-bar !-->
